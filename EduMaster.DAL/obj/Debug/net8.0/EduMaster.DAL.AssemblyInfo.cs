19212ebf2b70dadb3c52da5ebc76ddb70e02550e
Zangetsu001 <qdedmangg@gmail.com>
2025-11-24 14:49:59 +0200

    ss

--- EduMaster.DAL/obj/Debug/net8.0/EduMaster.DAL.AssemblyInfo.cs
+++ EduMaster.DAL/obj/Debug/net8.0/EduMaster.DAL.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("EduMaster.Domain")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EduMaster.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16bc26044690a73d0aa851c7409197822cfca427")]
-[assembly: System.Reflection.AssemblyProductAttribute("EduMaster.Domain")]
-[assembly: System.Reflection.AssemblyTitleAttribute("EduMaster.Domain")]
+[assembly: System.Reflection.AssemblyProductAttribute("EduMaster.DAL")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EduMaster.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
--- EduMaster.Services/obj/Debug/net8.0/EduMaster.Services.AssemblyInfo.cs
+++ EduMaster.Services/obj/Debug/net8.0/EduMaster.Services.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("EduMaster.DAL")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EduMaster.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10a2bb4cbcfdac8a66e7bf9817214eb6ecd9f9a8")]
-[assembly: System.Reflection.AssemblyProductAttribute("EduMaster.DAL")]
-[assembly: System.Reflection.AssemblyTitleAttribute("EduMaster.DAL")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16bc26044690a73d0aa851c7409197822cfca427")]
+[assembly: System.Reflection.AssemblyProductAttribute("EduMaster.Services")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EduMaster.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
